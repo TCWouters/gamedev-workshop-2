43199a37b15579c4454e356dc0691230bd2384e3
TCWouters <TC.Wouters1@student.han.nl>
2026-03-03 14:49:41 +0100

    Movement, watering can

---
+++ Assets/Scripts/Player/PickUpWateringCan.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PickUpWateringCan : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Capsule")
+        {
+            transform.parent = other.transform;
+            
+            // Position on the left side of the player
+            transform.localPosition = new Vector3(-0.5f, 0, 0);
+            
+            // Face forward
+            transform.localRotation = Quaternion.identity;
+        }
+    }
+    
+}
